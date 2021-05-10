8672ab52d73d5eab51b455bfb3aa9bd4a7191d15
EnderShadow8 <69184571+EnderShadow8@users.noreply.github.com>
2021-05-10 22:36:23 +0800

    Add html and ESM

--- catch.js
+++ catch.js
@@ -1,30 +1,29 @@
-let catchId;
-catchId= Math.floor(Math.random() *2+1);
-switch(catchId)
-{
-    case 1: 
-    
-        console.log("A wild rattata! Type 'throw' to catch it!")
-       
+export function startCatch() {
+  let catchId
+  catchId= Math.floor(Math.random() *2+1)
+  switch(catchId) {
+  case 1:
 
-        let catching;
-        let counter=0;
-        for(let i=0;i<12;i++)catching= Math.floor(Math.random() *15+1);
-        if(counter==10)
-        {
-            return 0;
-        }
-        if(catching<=13)
-        {
-            console.log("You caught a rattata!")
-            return 0;
-        }
-        else
-        {
-            console.log("You didn't catch it! Keep trying!")
-            counter++;
-        }
-    break;
-    case 2:
-        console.log("This is for now, run it again")
+    console.log("A wild rattata! Type 'throw' to catch it!")
+
+    let catching
+    let counter=0
+    for(let i=0;i<12;i++){
+      catching= Math.floor(Math.random() *15+1)
+    }
+    if(counter===10) {
+      return 0
+    }
+    if(catching<=13) {
+      console.log("You caught a rattata!")
+      return 0
+    }
+
+    console.log("You didn't catch it! Keep trying!")
+    counter++
+
+    break
+  case 2:
+    console.log("This is for now, run it again")
+  }
 }
---
+++ main.js
@@ -0,0 +1,1 @@
+import {startCatch} from "./catch"
